f11ea2063e4442248adfee185333caa2ecffe650
alessandro miele <alessandro.miele@decathlon.com>
2024-11-04 16:45:21 +0100

    feat: init project

---
+++ src/send.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+const fs = require('fs').promises;
+
+const templateName = process.argv[2];
+
+if (!templateName) {
+    console.error("No template name provided");
+    return;
+}
+
+(async () => {
+    const transporter = nodemailer.createTransport({
+        host: 'localhost',
+        port: 1025,
+        ignoreTLS: true
+    });
+
+    const emailTemplate = await fs.readFile(`src/templates/${templateName}.html`, 'utf8');
+
+    const mailOptions = {
+        from: 'test-sender@decathlon.com',
+        to: 'test-reciver@example.com',
+        subject: 'Test Email',
+        html: emailTemplate
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Email sent: ' + info.response);
+    });
+})()
